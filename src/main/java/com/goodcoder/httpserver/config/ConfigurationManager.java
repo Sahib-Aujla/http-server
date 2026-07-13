a650696de3dc2bf8595e27056e24d131e9638aba
Sahib.Aujla <sahibpreet568@gmail.com>
2026-07-13 12:18:47 -0300

    starting web server

---
+++ src/main/java/com/goodcoder/httpserver/config/ConfigurationManager.java
@@ -0,0 +1,20 @@
+package com.goodcoder.httpserver.config;
+
+public class ConfigurationManager {
+    private static ConfigurationManager myConfigurationManager;
+
+    private ConfigurationManager(){
+
+    }
+
+    public static ConfigurationManager getInstance(){
+        if(myConfigurationManager==null){
+            myConfigurationManager=new ConfigurationManager();
+        }
+        return myConfigurationManager;
+    }
+
+    public void loadConfigurationFile(String filePath){
+
+    }
+}
